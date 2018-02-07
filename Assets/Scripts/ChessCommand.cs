fdc20912b3ce3e0c290eab1b0799b79060396019
saporter_mac <scott.porter.ii@gmail.com>
2018-02-06 20:57:58 -0600

    interface for IChessPiece and Command pattern for movement / capture / bombs / etc. in place

---
+++ Assets/Scripts/ChessCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Abstract Chess Command. Will implement move, capture, or other creative game logic in these sub classes.  
+ * See IChessPiece for where invocation typically occurs.
+ * */
+public abstract class ChessCommand {
+    // Where this command should happen
+    public abstract int Location { get; }
+
+    // Command logic
+    public abstract void Execute(List<GameObject> board);
+}
---
+++ Assets/Scripts/IChessPiece.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Implemented by Chess Pieces so that GameManager can interact
+ * */
+public interface IChessPiece {
+    Affiliation Team { get; } 
+
+    // Returns all valid squares where this chess piece can move
+    List<int> AvailableMoves(List<GameObject> board, int currentPos);
+
+    // Notifies this chess piece that it has moved to a new position
+    List<ChessCommand> Moved(List<GameObject> board, int from, int to);
+
+    // Notifies this chess piece that it has been deleted
+    void OnDeath();
+}
+
+public enum Affiliation 
+{
+    White,
+    Black
+}
