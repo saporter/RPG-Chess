947cae714dce05391094038e0d5f8eb51883df76
saporter_mac <scott.porter.ii@gmail.com>
2018-02-17 14:26:51 -0600

    Pop up now toggles on and off

---
+++ Assets/Scripts/UI/PopUpWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpWindow : MonoBehaviour {
+    [SerializeField]
+    string PopUpType = "";
+
+	// Use this for initialization
+	private void Start () 
+    {
+        GameManager.Instance.PromotionEvent.AddListener(popUpListener);
+        toggleCanvasGroup(0f, false, false);
+	}
+
+    private void popUpListener(int location, string type)
+    {
+        if(type.Contains(PopUpType))
+        {
+            toggleCanvasGroup(1f, true, true);
+        }else
+        {
+            toggleCanvasGroup(0f, false, false);
+        }
+    }
+
+    private void toggleCanvasGroup(float alpha, bool interactable, bool blocksRaycasts)
+    {
+        CanvasGroup cg = gameObject.GetComponent<CanvasGroup>();
+        cg.alpha = alpha;
+        cg.interactable = interactable;
+        cg.blocksRaycasts = blocksRaycasts;
+    }
+
+
+    private void OnDestroy()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.PromotionEvent.RemoveListener(popUpListener);  // A good habit to get into
+        }
+    }
+}
