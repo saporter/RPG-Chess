ebfffc7f7c540cfa8f7443c7f70896510cf83781
saporter_mac <scott.porter.ii@gmail.com>
2018-02-23 17:13:25 -0600

    Sort of functional board setup

---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class Setup : MonoBehaviour {
+    public GameObject BoardSquares;
+
+	// Use this for initialization
+	void Start () {
+        GameObject squares = Instantiate(BoardSquares);
+        List<GameObject> board = new List<GameObject>();
+        foreach (Outline h in squares.transform.GetComponentsInChildren<Outline>())
+        {
+            board.Add(h.gameObject);
+        }
+
+        GameManager.Instance.ResetBoard(board);
+        squares.transform.SetParent(GameManager.Instance.transform);
+	}
+	
+}
---
+++ Assets/Scripts/UI/PieceSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceSelector : MonoBehaviour {
+
+    public int BoardIndex;
+    public Affiliation Team;
+    public string ChooserType;
+    public GameObject Chooser;
+    public Transform ChooserLocation;
+
+    public void Selected()
+    {
+        GameManager.Instance.PromotionEvent.Invoke(BoardIndex, (Team == Affiliation.White ? "White" : "Black") + ChooserType);
+    }
+}
